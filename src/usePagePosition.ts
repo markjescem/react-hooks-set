d0baec18cd6bb7e06763598685e115cfd782617f
mcuking <mcuking.tang@gmail.com>
2019-01-25 10:44:14 +0800

    fix: optimize usePagePostion method

--- src/usePagePosition.ts
+++ src/usePagePosition.ts
@@ -1,29 +1,37 @@
 import { useEffect } from 'react';
 import createStorage from './utils/createStorage';
 
-function usePagePosition(elementId: string) {
-  const storage = createStorage('local');
+function usePagePosition(
+  scollableElementId: string,
+  positionDataKey = 'react-hooks-use-page-positions',
+  storePositionEvenWindowClosed = false
+) {
+  const storage = storePositionEvenWindowClosed
+    ? createStorage('local')
+    : createStorage('session');
 
   useEffect(() => {
-    let pagePositions = storage.getItem('react-hooks-use-page-positions');
-    const scrollPage = elementId ? document.getElementById(elementId) : null;
-    const pathname = window.location.pathname;
+    let pagePositions = storage.getItem(positionDataKey);
+    const scrollPage = scollableElementId
+      ? document.getElementById(scollableElementId)
+      : null;
+    const href = window.location.href;
 
-    if (pagePositions[pathname]) {
+    if (pagePositions[href]) {
       if (scrollPage) {
-        scrollPage.scrollTop = pagePositions[pathname];
+        scrollPage.scrollTop = pagePositions[href];
       } else {
-        window.scrollTo(0, pagePositions[pathname]);
+        window.scrollTo(0, pagePositions[href]);
       }
     }
     return () => {
       pagePositions = {
         ...pagePositions,
         ...{
-          [pathname]: scrollPage ? scrollPage.scrollTop : window.scrollY
+          [href]: scrollPage ? scrollPage.scrollTop : window.scrollY
         }
       };
-      storage.setItem('react-hooks-use-page-positions', pagePositions);
+      storage.setItem(positionDataKey, pagePositions);
     };
   }, []);
 }
