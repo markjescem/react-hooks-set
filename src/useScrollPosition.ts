a9bcaf3b9cbade686e01b6c8dc3d82e2ced319a6
mcuking <mcuking.tang@gmail.com>
2019-01-28 20:25:20 +0800

    fix: rename usePagePostion to useScrollPostion

---
+++ src/useScrollPosition.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import createStorage from './utils/createStorage';
+
+interface Ref {
+  current: HTMLElement;
+}
+
+function useScrollPosition(
+  srcollRef: Ref,
+  positionDataKey = 'react-hooks-collection-scroll-positions',
+  storePositionEvenWindowClosed = false
+) {
+  const storage = storePositionEvenWindowClosed
+    ? createStorage('local')
+    : createStorage('session');
+
+  useEffect(() => {
+    let scrollPositions = storage.getItem(positionDataKey);
+    const href = window.location.href;
+    const scollContainer = srcollRef ? srcollRef.current : null;
+
+    if (scrollPositions[href]) {
+      if (scollContainer) {
+        scollContainer.scrollTop = scrollPositions[href];
+      } else {
+        window.scrollTo(0, scrollPositions[href]);
+      }
+    }
+    return () => {
+      scrollPositions = {
+        ...scrollPositions,
+        ...{
+          [href]: scollContainer ? scollContainer.scrollTop : window.scrollY
+        }
+      };
+      storage.setItem(positionDataKey, scrollPositions);
+    };
+  }, []);
+}
+
+export default useScrollPosition;
